46faf5ad7f3f45ec89a6a6c86483fc3542ee9a9b
Enzo Freitas <enzofreitasbh@gmail.com>
2026-06-10 14:41:55 -0300

    Delete

--- Lab10/quest3.py
+++
@@ -1,6 +0,0 @@
-soma = 0
-for i in range(10):
-    valor = int(input("Digite um valor inteiro positivo: "))
-    soma += valor
-
-print(f"A soma dos valores é: {soma}")
--- Lab10/quest6.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Entrada: "))
-
-print("Saída:\n")
-for i in range(1, 11):
-    print(f"{numero} X {i} = {numero * i}")
